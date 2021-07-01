ed7225786846b355373c71d300a018ca63954300
breannams <breannamarie_s@hotmail.com>
2021-06-30 20:18:10 -0400

    commit tree b5c310e3ff3c81eabafe2de79f9212021747dd4c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainId = document.getElementById("main");
+mainId.remove()
+
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
